b6a60df6a146e1f4342d4b5611b6100ea532378b
Simon Piscitelli <simonpiscitelli@gmail.com>
2022-03-30 16:08:28 +0100

    Update JSON validator message

--- src/validator/json.ts
+++ src/validator/json.ts
@@ -5,5 +5,5 @@ const validator = new jsonschema.Validator();
 export function json(val: any, schema: Schema): string[] {
     let result = validator.validate(val, schema);
 
-    return result.errors.map((err) => err.toString());
+    return result.errors.map((err) => err.toString().replace(`instance.`, ""));
 }
