66898dcc22502135ba8da8f3d5abfe769799a7bb
Simon Piscitelli <simonpiscitelli@gmail.com>
2023-07-23 21:09:29 +0100

    Merge items

--- src/json-schema/index.ts
+++ src/json-schema/index.ts
@@ -1,2 +1,52 @@
-export * from "./schema";
-export * from "./validator";
+// export * from "./schema";
+// export * from "./validator";
+
+import { TSchema, Type, Static } from "@sinclair/typebox";
+import jsonschema from "jsonschema";
+export { TSchema, Static };
+
+const validator = new jsonschema.Validator();
+
+interface ValidationError {
+    location: (string | number)[];
+    error: string;
+    type: string;
+}
+
+function validate(val: any, schema: TSchema): ValidationError[] {
+    let result = validator.validate(val, schema);
+
+    console.log(result.errors);
+
+    if (!result.errors.length) return [];
+
+    return result.errors.map((err) => {
+        return {
+            location: err.path,
+            error: err.message,
+            type: err.name,
+        };
+    });
+}
+
+function StringEnum<T extends string[]>(values: [...T]) {
+    return Type.Unsafe<T[number]>({ type: "string", enum: values });
+}
+
+function Nullable<T extends TSchema>(type: T) {
+    return Type.Union([type, Type.Null()]);
+}
+
+function ExtendedTypeBox() {
+    let extension = {
+        StringEnum,
+        Nullable,
+        validate,
+    };
+
+    let extended = Object.setPrototypeOf(extension, Type);
+
+    return extended as typeof Type & typeof extension;
+}
+
+export const JsonSchema = ExtendedTypeBox();
