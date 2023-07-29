4c2a07d1c34e86b618c0b295157b7845ec850cf3
Simon Piscitelli <simonpiscitelli@gmail.com>
2023-07-29 12:47:53 +0100

    Fix missing exports & Refactor

---
+++ src/json-schema/schema.ts
@@ -0,0 +1,41 @@
+import {
+    TSchema,
+    ExtendedTypeBuilder,
+    Type,
+    TUnion,
+    TNull,
+    TUnsafe,
+} from "@sinclair/typebox";
+import { validate } from "./validator";
+
+function StringEnum<T extends string[]>(values: [...T]): TUnsafe<T[number]> {
+    return Type.Unsafe<T[number]>({ type: "string", enum: values });
+}
+
+function Nullable<T extends TSchema>(type: T): TUnion<[T, TNull]> {
+    return Type.Union([type, Type.Null()]);
+}
+
+function Email() {
+    return Type.String({ format: "email" });
+}
+
+function DateExtended() {
+    return Type.Union([Type.Date(), Type.String({ format: "date-time" })]);
+}
+
+function ExtendedTypeBox() {
+    let extension = {
+        validate,
+        StringEnum,
+        Nullable,
+        Email,
+        DateExtended,
+    };
+
+    let extended = Object.setPrototypeOf(extension, Type);
+
+    return extended as typeof extension & ExtendedTypeBuilder;
+}
+
+export const Schema = ExtendedTypeBox();
