f9d48ee69686bbe6c00839de7d2346e951b278b4
Simon Piscitelli <simonpiscitelli@gmail.com>
2024-03-18 14:43:46 +0000

    Fix Exception contructors

--- src/error/index.ts
+++ src/error/index.ts
@@ -17,139 +17,140 @@ class Exception extends Error {
 
         this.name = name;
         this.code = code;
-        this.message = contextIsMessage ? context : message;
+        this.message =
+            contextIsMessage && context ? context : message ? message : name;
         this.context = contextIsMessage ? undefined : context;
     }
 }
 
 export function ValidationFailed(
-    context: Exception["context"],
+    context?: Exception["context"],
     message?: string,
 ): Exception {
     return new Exception("ValidationFailed", 400, message, context);
 }
 
 export function BadRequest(
-    context: Exception["context"],
+    context?: Exception["context"],
     message?: string,
 ): Exception {
     return new Exception("BadRequest", 400, message, context);
 }
 
 export function Unauthorized(
-    context: Exception["context"],
+    context?: Exception["context"],
     message?: string,
 ): Exception {
     return new Exception("Unauthorized", 401, message, context);
 }
 
 export function PaymentRequired(
-    context: Exception["context"],
+    context?: Exception["context"],
     message?: string,
 ): Exception {
     return new Exception("PaymentRequired", 402, message, context);
 }
 
 export function Forbidden(
-    context: Exception["context"],
+    context?: Exception["context"],
     message?: string,
 ): Exception {
     return new Exception("Forbidden", 403, message, context);
 }
 
 export function NotFound(
-    context: Exception["context"],
+    context?: Exception["context"],
     message?: string,
 ): Exception {
     return new Exception("NotFound", 404, message, context);
 }
 
 export function NotAcceptable(
-    context: Exception["context"],
+    context?: Exception["context"],
     message?: string,
 ): Exception {
     return new Exception("NotAcceptable", 406, message, context);
 }
 
 export function TimeOut(
-    context: Exception["context"],
+    context?: Exception["context"],
     message?: string,
 ): Exception {
     return new Exception("TimeOut", 408, message, context);
 }
 
 export function Conflict(
-    context: Exception["context"],
+    context?: Exception["context"],
     message?: string,
 ): Exception {
     return new Exception("Conflict", 409, message, context);
 }
 
 export function Gone(
-    context: Exception["context"],
+    context?: Exception["context"],
     message?: string,
 ): Exception {
     return new Exception("Gone", 410, message, context);
 }
 
 export function PreconditionFailed(
-    context: Exception["context"],
+    context?: Exception["context"],
     message?: string,
 ): Exception {
     return new Exception("PreconditionFailed", 412, message, context);
 }
 
 export function PayloadTooLarge(
-    context: Exception["context"],
+    context?: Exception["context"],
     message?: string,
 ): Exception {
     return new Exception("PayloadTooLarge", 413, message, context);
 }
 
 export function URITooLong(
-    context: Exception["context"],
+    context?: Exception["context"],
     message?: string,
 ): Exception {
     return new Exception("URITooLong", 414, message, context);
 }
 
 export function UnsupportedMediaType(
-    context: Exception["context"],
+    context?: Exception["context"],
     message?: string,
 ): Exception {
     return new Exception("UnsupportedMediaType", 415, message, context);
 }
 
 export function ExpectationFailed(
-    context: Exception["context"],
+    context?: Exception["context"],
     message?: string,
 ): Exception {
     return new Exception("ExpectationFailed", 417, message, context);
 }
 
 export function UnprocessableEntity(
-    context: Exception["context"],
+    context?: Exception["context"],
     message?: string,
 ): Exception {
     return new Exception("UnprocessableEntity", 422, message, context);
 }
 
 export function TooManyRequests(
-    context: Exception["context"],
+    context?: Exception["context"],
     message?: string,
 ): Exception {
     return new Exception("TooManyRequests", 429, message, context);
 }
 
 export function InternalError(
-    context: Exception["context"],
+    context?: Exception["context"],
     message?: string,
 ): Exception {
     return new Exception("InternalError", 500, message, context);
 }
 
 export function NotImplemented(
-    context: Exception["context"],
+    context?: Exception["context"],
     message?: string,
 ): Exception {
     return new Exception("NotImplemented", 501, message, context);
