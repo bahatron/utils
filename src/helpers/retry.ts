f63fb1c055bb8df5016da354f6924c9e4665e4c1
Simon Piscitelli <simonpiscitelli@gmail.com>
2024-05-28 16:13:34 +0100

    Add description and extra retry options

--- src/helpers/retry.ts
+++ src/helpers/retry.ts
@@ -1,12 +1,30 @@
+import { sleep } from "./sleep";
+
 export interface RetryOptions {
     tries?: number;
+    timeout?: number;
+    factor?: number;
 }
 
-export function retry<T>(handler: () => T, { tries = 3 }: RetryOptions = {}) {
+/**
+ * @param timeout in milliseconds, defaults to 0
+ * @param factor integer for exponential backoff, defaults to 1
+ * @param tries defaults to 3
+ */
+export function retry<T>(
+    handler: () => T,
+    { tries = 3, timeout = 0, factor = 1 }: RetryOptions = {},
+) {
     let retrier = async (tryNumber = 0): Promise<T> => {
         try {
             return await handler();
         } catch (err) {
+            let waitTime = Math.floor(
+                100 * Math.random() + timeout * Math.pow(factor, tryNumber + 1),
+            );
+
+            await sleep(waitTime);
+
             if (tryNumber >= tries) throw err;
 
             return retrier(tryNumber + 1);
--- src/helpers/sleep.ts
+++ src/helpers/sleep.ts
@@ -1,3 +1,6 @@
+/**
+ * @description time in milliseconds
+ */
 export async function sleep(timer: number) {
     await new Promise((resolve) => setTimeout(resolve, timer));
 }
