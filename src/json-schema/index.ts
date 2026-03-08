1a2d177181febcf708d54071b6cc66e89d4da055
Simon Piscitelli <simonpiscitelli@gmail.com>
2026-03-08 11:59:18 +0000

    Add more needed exports

--- src/json-schema/index.ts
+++ src/json-schema/index.ts
@@ -1,3 +1,4 @@
 export type * from "@sinclair/typebox";
+export { OptionalKind, ReadonlyKind } from "@sinclair/typebox";
 export { Schema as JsonSchema } from "jsonschema";
 export * from "./schema";
